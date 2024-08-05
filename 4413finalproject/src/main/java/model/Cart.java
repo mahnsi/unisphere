97ef946b805bfa455f45f7151552156a0e3eec13
Mahnsi <mahnsir22@gmail.com>
2024-08-04 20:04:26 -0400

    model updates

--- 4413finalproject/src/main/java/model/Cart.java
+++ 4413finalproject/src/main/java/model/Cart.java
@@ -1,18 +1,38 @@
 package model;
-
+import java.util.HashMap;
+import java.util.Map;
 import java.util.List;
 
 public class Cart {
 	User owner;
 	float total_price;
-	List <Product> items;
+	Map <Product, Integer> items;
 	
 	public Cart() {
+		items =  new HashMap<>();
+
 		
 	}
 	
 	public void add(Product product) {
-		items.add(product);
+		
+		if (items.containsKey(product)) {
+			items.put(product, items.get(product) + 1);
+			return;
+        }
+		
+		items.put(product, 1);
+	}
+	
+	public void remove(Product product) {
+		//entirley remove
+		items.remove(product);
+		
+	}
+	
+	public void updateQuantity(Product product, int x) {
+		items.put(product, x);
+		
 	}
 
 }
